99439cc41def91bf7ba7bdf298be9c3fd786fc54
enkosixalisa <enkosixalisa@gmail.com>
2012-05-22 13:58:34 +0000

    modified Technical Staff Module

--- NewHospital/src/java/com/cput/hospital/app/Modules/TechnicalStaff/TechnicalStaffCrudService.java
+++ NewHospital/src/java/com/cput/hospital/app/Modules/TechnicalStaff/TechnicalStaffCrudService.java
@@ -2,9 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.cput.hospital.services;
+package com.cput.hospital.app.Modules.TechnicalStaff;
 
 import com.cput.hospital.model.TechnicalStaff;
+import com.cput.hospital.services.Service;
 
 /**
  *
